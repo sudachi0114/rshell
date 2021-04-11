83e57b52145559789c0c535699a99abd6c534633
sudachi0114 <jnevariste@gmail.com>
2021-04-11 10:46:22 +0900

    create readline loop, read from stdin and print stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+        stdin().read_line(&mut line).expect("Failed to read line.");
+        print!("{}", line);
+    }
 }
