9f5238e1858a19269eea1011650ad1d82ed8d9d3
sudachi0114 <jnevariste@gmail.com>
2021-04-11 10:58:15 +0900

    modified to be more prompt-like

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::io::stdin;
+use whoami;
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     loop {
+        print!("{} $ ", whoami::username());
+        stdout().flush().unwrap();
         let mut line = String::new();
         stdin().read_line(&mut line).expect("Failed to read line.");
         print!("{}", line);
