22e3e7cae0ec5117b7d952acc80e0ae8033881d8
sudachi0114 <jnevariste@gmail.com>
2021-04-11 11:02:03 +0900

    modify prompt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use whoami;
+use std::env;
 use std::io::{stdin, stdout, Write};
 
 fn main() {
     loop {
-        print!("{} $ ", whoami::username());
+        print!("{}@{}:{} $ ", whoami::username(), whoami::hostname(), env::current_dir().unwrap().display());
         stdout().flush().unwrap();
         let mut line = String::new();
         stdin().read_line(&mut line).expect("Failed to read line.");
