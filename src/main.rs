e8b0e2c0f15a38e2fee5ff8ca27abc3096ad43db
sudachi0114 <jnevariste@gmail.com>
2021-04-11 11:08:40 +0900

    implement exit

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,14 @@ fn main() {
     loop {
         print!("{}@{}:{} $ ", whoami::username(), whoami::hostname(), env::current_dir().unwrap().display());
         stdout().flush().unwrap();
+
         let mut line = String::new();
         stdin().read_line(&mut line).expect("Failed to read line.");
-        print!("{}", line);
+        line.remove(line.len() - 1);
+        println!("(debug) {}", line);
+
+        if line == "exit" {
+            return;
+        }
     }
 }
